bba75aaf4366584f36a3ea4b5559a02f35a19c23
tazz0009 <tazz0009@gmail.com>
2021-05-15 21:49:37 +0900

    Database Connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -30,3 +31,14 @@ def show(id: int):
 @app.get('/blog/{id}/comments')
 def show(id: int):
     return {'data': {'blog': [1, 2]}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
